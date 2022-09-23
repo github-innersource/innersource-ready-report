0575f139e7bce1848c7ec3cb983d116f90e511c3
Jürgen Efeish <jefeish@github.com>
2022-09-22 20:14:43 -0400

    added report checks

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
 const ui = require('./ui/innersourceUI.js')
 const fs = require('fs')
 const yaml = require('js-yaml')
+const util = require('util')
+let innersourceRequirements = {}
+let report = ""
 
 /**
  * This function loads the app configuration from the file system
@@ -11,29 +14,135 @@ function loadAppConfig(app) {
   try {
     // eslint-disable-next-line no-path-concat
     const configData = fs.readFileSync('./src/innersource-checklist.yml', 'utf8')
-    jsonConfig = JSON.parse(JSON.stringify(yaml.load(configData), null, 4))
-
-    app.log.info('Loading App Config file')
+    return JSON.parse(JSON.stringify(yaml.load(configData), null, 4))
   } catch (err) {
     console.error('Error loading App Config file: ', err)
   }
 }
 
 /**
+ * Generic function to check if a file exists in the repository
+ * @param {*} fileName 
+ */
+async function checkContent(app, context, fileName) {
+  let response = ""
+
+  try {
+    const file = await context.octokit.repos.getContent(
+      {
+        owner: context.payload.repository.owner.login,
+        repo: context.payload.repository.name,
+        path: fileName
+      }
+    );
+
+    let buff = Buffer.from(file.data.content, 'base64');
+    let text = buff.toString('utf-8');
+
+    response = text
+  } catch (err) {
+    response = err.status
+  }
+
+  return response
+}
+
+/**
+ * 
+ * @param {*} app 
+ */
+async function checkForFile(app, context, fileName) {
+  app.log.info("checkForFile: " + fileName)
+  let response
+  let line
+
+  response = await checkContent(app, context, fileName)
+  if (response == "404") {
+    response = await checkContent(app, context, ".github/" + fileName)
+    if (response == "404") {
+      response = "|:warning:| " + fileName + "| File Not found |\n"
+    } else {
+      line = response
+      response = "|:white_check_mark:| " + fileName + "| .github/" + fileName + " |\n"
+    }
+  }
+  else {
+    line = response
+    response = "|:white_check_mark:|" + fileName + "| /" + fileName + "|\n"
+  }
+
+  return response
+}
+
+/**
+ * 
+ * @param {*} app 
+ */
+async function branch_protection(app, context, branch_protection_rules) {
+  app.log.info("Branch Protection:" + util.inspect(branch_protection_rules))
+  let response = "\n\n## Branch Protection Rules\n\n"
+  response += "|STATUS|RULE|EXPECTED|FOUND|\n|---|---|---|---|\n"
+
+  Object.keys(branch_protection_rules).forEach(async (ruleName) => {
+    response += "|:white_check_mark:|" + ruleName + "|" + branch_protection_rules[ruleName] + "||\n"
+    app.log.info("rule:" + branch_protection_rules[ruleName])
+  })
+
+  return response
+}
+
+/**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
  */
-module.exports = (app, { getRouter })=> {
+module.exports = (app, { getRouter }) => {
   // Your code here
   app.log.info("Yay, the app was loaded!");
 
-  loadAppConfig(app)
-  
   app.on("repository.edited", async (context) => {
+
+    innersourceRequirements = loadAppConfig(app)
+    report = "# Innersource Onboarding Report\n\n## Required Files\n\n"
     app.log.info("repository.edited");
+
     if (context.payload.changes.topics) {
+      app.log.info("repository.edited");
       app.log.info("Topics changed");
       app.log.info("Topics: " + context.payload.repository.topics)
+
+      report += "\n\n|STATUS|FILE|LOCATION|\n|---|---|---|\n"
+      if (innersourceRequirements['license']) {
+        report += await checkForFile(app, context, "LICENSE")
+      }
+
+      if (innersourceRequirements['code_of_conduct'] === true) {
+        report += await checkForFile(app, context, "CODE_OF_CONDUCT")
+      }
+
+      if (innersourceRequirements['codeowners'] === true) {
+        report += await checkForFile(app, context, "CODEOWNERS")
+      }
+
+      if (innersourceRequirements['contributing'] === true) {
+        report += await checkForFile(app, context, "CONTRIBUTING")
+      }
+
+      if (innersourceRequirements['readme'] === true) {
+        report += await checkForFile(app, context, "README.md")
+      }
+
+      if (innersourceRequirements['branch_protection_rules']) {
+        report += await branch_protection(app, context, innersourceRequirements['branch_protection_rules'])
+      }
+
+      app.log.info("report: " + report)
+
+      const issue = await context.octokit.rest.issues.create({
+        owner: context.payload.repository.owner.login,
+        repo: context.payload.repository.name,
+        title: 'Innersource Onboarding Report',
+        body: report,
+      });
     }
   });
 
